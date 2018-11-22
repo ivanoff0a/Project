964879ea1c47f25e82a9bea161d215b100c2f450
unknown <ivanov-andrey15@mail.ru>
2018-11-22 21:03:20 +0300

    second commit

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
+	let addPurposeBtn = $('.js-addPurpose');
 	
+	interfaceBuilder.buildInterface('firstScreen');
+
+	$('body').on('click', '.addPurposeBtn', function() {
+
+	});
 });
