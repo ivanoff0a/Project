9242403f0673cb355547ca2eaa666ff15b7f7ccb
unknown <ivanov-andrey15@mail.ru>
2018-11-26 20:57:39 +0300

    third commit

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,25 @@
 $(document).ready(function() {
 	let addPurposeBtn = $('.js-addPurpose');
+	let userProjects = []
 	
 	interfaceBuilder.buildInterface('firstScreen');
 
-	$('body').on('click', '.addPurposeBtn', function() {
+	$('body').on('click', '.js-addPurpose', function() {
+		modalService.openModal('newGoal');
+	});
 
+	$('body').on('click', '.modal__bg', function() {
+		$('.modal').remove();	
+	});
+
+
+	$('body').on('click', '.js-createGoal', function() {
+		userProjects.push({
+			userName: $('body').find('.js-newUserName').val(), 
+			projectName: $('body').find('.js-newProjectName').val(),
+			projectDuration: $('body').find('.js-newProjectDuration').val(),
+		})
+		$('.modal').remove();
+		console.log(userProjects);
 	});
 });
