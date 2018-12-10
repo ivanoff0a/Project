8dc5b47a82f4b88c1bccff0c167bcb6d35682cf9
unknown <ivanov-andrey15@mail.ru>
2018-12-10 20:56:45 +0300

    sixth commit

--- assets/scripts/interfaceBuilder.js
+++ assets/scripts/interfaceBuilder.js
@@ -51,8 +51,8 @@ interfaceBuilder.interfaces = {
 						</div>\
 					</div>\
 					<div class="goal__elem goal__path">\
-						<div class="goal__action js-addCheckpoint">\
-							<svg class="goal__action__img" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" \
+						<div class="goal__action">\
+							<svg class="goal__action__img js-addCheckpoint" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" \
 							y="0px"	 width="510px" height="510px" viewBox="0 0 510 510" style="enable-background:new 0 0 510 510;" xml:space="preserve">\
 							<g>\
 								<g id="add-circle">\
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -39,10 +39,17 @@ $(document).ready(function() {
 		$('.modal').remove();
 		console.log(userProjects[0]);
 		interfaceBuilder.build('goalScreen');
-		$('.goal__path').animate('width', ($('.goal__path').width() + 120) + 'px');
-		$('.goal__path').animate('justify-content', 'space-around');
-		$('.goal__action').append(
-			''
+		$('.js-textProjectName').text(userProjects[userProjects.length - 1].projectName);
+		$('.goal__path').css('width', ($('.goal__path').width() + 120) + 'px');
+		$('.goal__path').css('justify-content', 'space-around');
+		$('.goal__path').append(
+			'<div class="goal__action__checked">\
+				<svg version="1.1" class="goal__action__checked__img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 44 44" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 44 44">\
+					<path d="m22,0c-12.2,0-22,9.8-22,22s9.8,22 22,22 22-9.8 22-22-9.8-22-22-22zm12.7,15.1l0,0-16,16.6c-0.2,0.2-0.4,0.3-0.7,0.3-0.3,0-0.6-0.1-0.7-0.3l-7.8-8.4-.2-.2c-0.2-0.2-0.3-0.5-0.3-0.7s0.1-0.5 0.3-0.7l1.4-1.4c0.4-0.4 1-0.4 1.4,0l.1,.1 5.5,5.9c0.2,0.2 0.5,0.2 0.7,0l13.4-13.9h0.1c0.4-0.4 1-0.4 1.4,0l1.4,1.4c0.4,0.3 0.4,0.9 0,1.3z"/>\
+				</svg>\
+			</div>'
 		);
-	});	
+		$('body').find('.goal__action__checked').css('right', '+=70');
+		$('body').find('.goal__action').css('left', '+=70');
+	});
 });
