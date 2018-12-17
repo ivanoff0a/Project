55b78b3a100419c913bd2ffd57c5d48ddb30cf0e
ivanoff0a <ivanov-andrey15@mail.ru>
2018-12-17 18:18:33 +0300

    b

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -44,15 +44,23 @@ $(document).ready(function() {
 		$('.js-textProjectName').text(userProjects[userProjects.length - 1].projectName);
 		$('.goal__path').css('width', ($('.goal__path').width() + 80) + 'px');
 		$('.goal__path').append(
-			'<div class="goal__action__checked">\
-				<svg version="1.1" class="goal__action__checked__img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 44 44" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 44 44">\
-					<path d="m22,0c-12.2,0-22,9.8-22,22s9.8,22 22,22 22-9.8 22-22-9.8-22-22-22zm12.7,15.1l0,0-16,16.6c-0.2,0.2-0.4,0.3-0.7,0.3-0.3,0-0.6-0.1-0.7-0.3l-7.8-8.4-.2-.2c-0.2-0.2-0.3-0.5-0.3-0.7s0.1-0.5 0.3-0.7l1.4-1.4c0.4-0.4 1-0.4 1.4,0l.1,.1 5.5,5.9c0.2,0.2 0.5,0.2 0.7,0l13.4-13.9h0.1c0.4-0.4 1-0.4 1.4,0l1.4,1.4c0.4,0.3 0.4,0.9 0,1.3z"/>\
-				</svg>\
-			</div>'
+			'<div class="goal__action__checked__container">\
+				<div class="goal__action__checked__info">\
+				</div>\
+				<div class="goal__action__checked">\
+					<svg version="1.1" class="goal__action__checked__img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 44 44" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 44 44">\
+						<path d="m22,0c-12.2,0-22,9.8-22,22s9.8,22 22,22 22-9.8 22-22-9.8-22-22-22zm12.7,15.1l0,0-16,16.6c-0.2,0.2-0.4,0.3-0.7,0.3-0.3,0-0.6-0.1-0.7-0.3l-7.8-8.4-.2-.2c-0.2-0.2-0.3-0.5-0.3-0.7s0.1-0.5 0.3-0.7l1.4-1.4c0.4-0.4 1-0.4 1.4,0l.1,.1 5.5,5.9c0.2,0.2 0.5,0.2 0.7,0l13.4-13.9h0.1c0.4-0.4 1-0.4 1.4,0l1.4,1.4c0.4,0.3 0.4,0.9 0,1.3z"/>\
+					</svg>\
+				</div>\
+			 </div>'
 		);
 		let c = userProjects[0].checkpoints;
 		let last = $('body').find('.goal__action__checked').length - 1; 
 		$('body').find('.goal__action__checked').eq(last).css('left', c[c.length - 1].positionActionChecked + 'px');
 		$('body').find('.goal__action').eq(last).css('right', c[c.length - 1].positionAction + 'px');
 	});
+
+	$('body').on('click', '.goal__action__checked', function() {
+
+	});	
 });
